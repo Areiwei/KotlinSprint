09ada46c5c4ea98b4ebfbfafbf7d2b263d81286f
Lordi <bigrous2@mail.ru>
2024-03-06 21:34:44 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main(){
+    val countOrder: Int = 75
+    val thxPurchase: String = "Благодарим за заказ! "
+    println(countOrder)
+    println(thxPurchase)
+var countMembers = 2000
+//    print(countMembers)
+    println(--countMembers)
+}
