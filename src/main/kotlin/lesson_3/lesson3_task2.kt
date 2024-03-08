6ddf13a1fcf83e9c2f4057953dde190e7f3f0c44
Lordi <bigrous2@mail.ru>
2024-03-09 00:41:26 +0300

    KS-3-2

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+    var family = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$family $name $patronymic, $age")
+
+    family = "Сидорова"
+    age = 22
+    println("$family $name $patronymic, $age")
+}
