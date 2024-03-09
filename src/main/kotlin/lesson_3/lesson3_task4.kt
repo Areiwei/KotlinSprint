7bdb0388b3fc33bfd6319bf6f1a958312f0baee5
Lordi <bigrous2@mail.ru>
2024-03-09 14:24:35 +0300

    KS-3-4

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var move = 1
+    var from = "E2"
+    var to = "E4"
+
+    println("$from-$to;$move")
+
+    move = 2
+    from = "D2"
+    to = "D3"
+
+    println("$from-$to;$move")
+}
