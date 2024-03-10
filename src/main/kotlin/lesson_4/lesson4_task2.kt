47be734af1a9d8340a2fe947cdb8c937da2a8ea8
Lordi <bigrous2@mail.ru>
2024-03-10 14:27:40 +0300

    KS-4-2 capacity calculations corrected, line size changed

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -6,8 +6,16 @@ fun main() {
     val cargoWeight2 = 50
     val cargoCapacity2 = 100
 
-    println("Груз с весом $cargoWeight1 кг и объемом $cargoCapacity1 соотвествует категории 'Average':${(cargoWeight1 >= MIN_WEIGHT) && (cargoWeight1 <= MAX_WEIGHT) && (cargoCapacity1 <= CAPACITY)}")
-    println("Груз с весом $cargoWeight2 кг и объемом $cargoCapacity2 соотвествует категории 'Average':${(cargoWeight2 >= MIN_WEIGHT) && (cargoWeight2 <= MAX_WEIGHT) && (cargoCapacity2 <= CAPACITY)}")
+    println("Груз с весом $cargoWeight1 кг и объемом $cargoCapacity1 соотвествует категории" +
+            " 'Average':" +
+                   "${(cargoWeight1 >= MIN_WEIGHT)
+                    && (cargoWeight1 <= MAX_WEIGHT)
+                    && (cargoCapacity1 < CAPACITY)}")
+    println("Груз с весом $cargoWeight2 кг и объемом $cargoCapacity2 соотвествует категории " +
+            "'Average':" +
+                    "${(cargoWeight2 >= MIN_WEIGHT) 
+                    && (cargoWeight2 <= MAX_WEIGHT)
+                    && (cargoCapacity2 < CAPACITY)}")
 }
 const val MAX_WEIGHT = 100
 const val MIN_WEIGHT = 35
