5e8006f9b0ec8c80d857d8b887ab50e6154a47e4
Lordi <bigrous2@mail.ru>
2024-03-11 19:41:16 +0300

    KS-4-1 the code was rewritten more accurately taking into account the technical specifications

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,10 +1,11 @@
 package org.example.lesson_4
 
 fun main() {
-    var tablesToday = 0
-    var tablesTomorrow = 4
+    var tablesToday = 13
+    var tablesTomorrow = 9
 
-    val resultToday = (1 <= tablesToday)
-    val resultTomorrow = (1 <= tablesTomorrow)
-    println("Доступность столиков на сегодня:$resultToday\nДоступность столиков на завтра:$resultTomorrow")
+    val resultToday = (TABLES > tablesToday)
+    val resultTomorrow = (TABLES > tablesTomorrow)
+    println("Доступность столиков на сегодня: $resultToday\nДоступность столиков на завтра: $resultTomorrow")
 }
+const val TABLES = 13
