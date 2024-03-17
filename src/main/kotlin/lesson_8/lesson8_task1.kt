3d7f9e4c526be1f3f8c9dbc141e24e846c88a109
Lordi <bigrous2@mail.ru>
2024-03-17 17:54:19 +0300

    KS-8-3

---
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -0,0 +1,6 @@
+package org.example.lesson_8
+
+fun main() {
+    val viewsArray = intArrayOf(307, 340, 318, 344, 469, 545, 420)
+    println("Количество просмотров за неделю: ${viewsArray.sum()}")
+}
