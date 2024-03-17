52e449c4671bfe2ba03d1f924ab5599c17cce34e
Lordi <bigrous2@mail.ru>
2024-03-17 11:23:24 +0300

    KS-6-2 The code has been simplified and modernized

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -3,14 +3,9 @@ package org.example.lesson_6
 fun main() {
 
     println("Введите требуемое количество секунд")
-    val secondsList = mutableListOf<Int>()
 
     var input: Int = readln().toInt()
-    secondsList.add(input)
+    Thread.sleep(input * 1000L)
 
-    while (input > 0) {
-        input--
-        Thread.sleep(1000)
-    }
-    println("Прошло $secondsList секунд")
+    println("Прошло $input секунд")
 }
