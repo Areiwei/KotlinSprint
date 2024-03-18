d69ca72317a2599f508d0ce473540f7ab0941cfc
Lordi <bigrous2@mail.ru>
2024-03-18 20:41:11 +0300

    KS-7-1 the code is simplified

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++ src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -4,13 +4,10 @@ fun main() {
 
     println("Введите число")
     val inputNumber = readln().toInt()
-    val number = inputNumber
 
-    val numbRange = 0..2147483647 step 2
-
-    for (i in numbRange) {
+    for (i in 0..inputNumber step 2) {
         println(i)
-        if (i == number || i == number - 1)
+        if (i == inputNumber || i == inputNumber - 1)
             break
 
     }
