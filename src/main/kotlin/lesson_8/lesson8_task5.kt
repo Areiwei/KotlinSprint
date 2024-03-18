f97c50f14dcde9a08aa498b243ea2fc26d9ec15e
Lordi <bigrous2@mail.ru>
2024-03-18 23:16:41 +0300

    KS-8-5 variable name changed

---
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_8
+
+fun main() {
+
+    println("Сколько ингредиентов планируется ввести?")
+    val howIngredients = readln().toInt()
+
+    val ingredientsArray = arrayOfNulls<String>(howIngredients)
+    var currentIndex = 0
+
+    println("Введите по очереди наименование ингредиентов")
+    repeat(howIngredients) {
+        val inputIngredients = readln()
+        ingredientsArray[(currentIndex)] = inputIngredients
+        currentIndex++
+    }
+    println(ingredientsArray.joinToString())
+
+}
