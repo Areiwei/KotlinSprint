75a2462e1b372bc00b17d576ce825289946823b1
Lordi <bigrous2@mail.ru>
2024-03-20 20:14:45 +0300

    KS-9-2 case is ignored when entering

--- src/main/kotlin/lesson_9/lesson9_task2.kt
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -2,18 +2,21 @@ package org.example.lesson_9
 
 fun main() {
 
-    val listOfIngredients = mutableListOf<String>("котлета", "сыр", "кетчуп")
+    val listOfIngredients = mutableListOf("котлета", "сыр", "кетчуп")
     println("В рецепте есть базовые ингредиенты: $listOfIngredients")
 
     println("Желаете добавить ещё?(да/нет)")
     val moreIngredients = readln()
-    if
-            (moreIngredients == "да") {
+    val yes1 = "да"
+    val yes2 = "Да"
+    if (moreIngredients.equals(yes1, ignoreCase = true) ||
+        moreIngredients.equals(yes2, ignoreCase = true)
+    ) {
         println("Какой ингредиент вы хотите добавить?")
     } else {
         return
     }
-   val newIngredient = readln()
+    val newIngredient = readln()
     listOfIngredients.add(newIngredient)
     println("Теперь в рецепте есть следующие ингредиенты: $listOfIngredients")
 }
