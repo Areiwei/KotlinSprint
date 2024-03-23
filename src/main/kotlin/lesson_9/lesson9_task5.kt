9428f8bcbee43754116d8e1450f21530dd6c436b
Lordi <bigrous2@mail.ru>
2024-03-23 21:39:49 +0300

    KS-9-5 methods changed, collection type changed, code modernized

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -2,17 +2,15 @@ package org.example.lesson_9
 
 fun main() {
 
-    val listOfIngredients = mutableListOf<String>()
+    val listOfIngredients = mutableSetOf<String>()
     println("Введите пять ингредиентов (каждый по отдельности).")
     repeat(5) {
-        val inputIngredients = readln()
+        val inputIngredients = readln().lowercase()
         listOfIngredients.add(inputIngredients)
     }
 
-    val orderedListOfIngredients = listOfIngredients.distinct().sorted()
-    val result = orderedListOfIngredients.mapIndexed { index, ingredient ->
-        if (index == 0) ingredient.capitalize() else ingredient
-    }.joinToString(", ")
+    val orderedListOfIngredients = listOfIngredients.sorted().toMutableList()
+    orderedListOfIngredients[0] = orderedListOfIngredients[0].replaceFirstChar { it.titlecase() }
+    val result = orderedListOfIngredients.joinToString(", ")
     println(result)
 }
-
