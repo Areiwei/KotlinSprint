b202955f36d56fa60348afa26c30049570bb4964
Lordi <bigrous2@mail.ru>
2024-04-08 23:03:39 +0300

    KS-11-4 added recipes list, renamed some fun

--- src/main/kotlin/lesson_11/lesson11_task4.kt
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -2,33 +2,46 @@ package org.example.lesson_11
 
 fun main() {
     val burgers = RecipeCategory(1, "Бургеры", "Рецепты всех популярных видов бургеров")
-    val burger3 = Burgers("Бургер с грибами и сыром")
+    val burger3 = Recipe("Бургер с грибами и сыром", 3, "Творог, куриное яйцо, мука, сахар, мясо")
     val burger3Compound = Ingredient(12, "Куриное яйцо", "2")
+
+    val soups = RecipeCategory(2, "Супы", "Рецепты вкусных супов")
+    val borsch = Recipe("Борщ", 22, "Картофель, свекла, капуста, морковь, зелень, соль")
+    val pea = Recipe("Гороховый", 23, "Картофель, горох, лук, морковь, ребрышки, соль")
     burgers.choiceCategory()
-    burger3.openBurger()
+    burger3.choiceRecipe()
     burger3Compound.showInfo()
+    soups.addRecipes(borsch)
+    soups.addRecipes(pea)
 }
 
 class RecipeCategory(
-    val recipeId: Int?,
+    val categoryId: Int,
     val name: String,
     val about: String,
+    val recipes: MutableList<Recipe> = mutableListOf()
 ) {
     fun choiceCategory() {
         println("Выбрана категория $name")
     }
+
+    fun addRecipes(recipe: Recipe) {
+        recipes.add(recipe)
+    }
 }
 
-class Burgers(
-    val burgerName: String,
+class Recipe(
+    val recipeName: String,
+    val recipeId: Int,
+    val ingredients: String,
 ) {
-    fun openBurger() {
-        println(burgerName)
+    fun choiceRecipe() {
+        println(recipeName)
     }
 }
 
 class Ingredient(
-    val ingredientId: Int?,
+    val ingredientId: Int,
     val ingredientName: String,
     val ingredientAmount: String,
 ) {
